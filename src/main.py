c5ac5bcd3f0318e633740f6d731cf662abc49d59
naoto-kawakami <16462088+naoto-kawakami@users.noreply.github.com>
2025-08-11 14:44:14 +0900

    fix: Fix config yaml file path.

--- src/main.py
+++ src/main.py
@@ -8,9 +8,12 @@ from configs import PersonConfig
 if __name__ == "__main__":
     # Example usage
     # Assuming the YAML file is structured correctly.
-    filepath = Path("../config/person-config.yaml").expanduser().resolve()
-    if not filepath.exists():
-        raise FileNotFoundError(f"Configuration file not found: {filepath}")
+    src_dir = Path(__file__).expanduser().resolve().parent
+    project_dir = src_dir.parent
+    config_dir = project_dir / "config"
+    config_filepath = config_dir / "person-config.yaml"
+    if not config_filepath.exists():
+        raise FileNotFoundError(f"Configuration file not found: {config_filepath}")
 
-    person_config = PersonConfig.from_yaml("../config/person-config.yaml")
+    person_config = PersonConfig.from_yaml(config_filepath)
     print(person_config)
